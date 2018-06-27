a9651058e339ed502e57c0522adaa0e350859d1b
Tomi Paul <tomipaul95@gmail.com>
2018-06-28 00:06:49 +0100

    feat(docker): Update docker and application setup

--- a_socials/settings/base.py
+++ a_socials/settings/base.py
@@ -25,6 +25,7 @@ dotenv.load()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = dotenv.get('DEBUG', True)
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
