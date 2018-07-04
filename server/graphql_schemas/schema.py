01409683d0027dd2225da9fdd719e009329f10d8
David Maitoyo <davidmaitoyo@Davids-MacBook-Pro.local>
2018-07-04 16:20:53 +0300

    Feature Setting Up GrapghQl

--- server/graphql_schemas/schema.py
+++ server/graphql_schemas/schema.py
@@ -1,15 +1,21 @@
 import graphene
 from graphene_django.debug import DjangoDebug
 
+from .category.schema import Query as CategoryQuery
+from .interest.schema import Query as InterestQuery
 
 
-class Query(graphene.ObjectType):
-    debug = graphene.Field(DjangoDebug, name='__debug')
-    pass
+class Query(
+  CategoryQuery,
+  InterestQuery,
+  graphene.ObjectType,
+):
+  debug = graphene.Field(DjangoDebug, name='__debug')
+  pass
 
 
 class Mutation(graphene.ObjectType):
-    pass
+  pass
 
 
 schema = graphene.Schema(query=Query)
