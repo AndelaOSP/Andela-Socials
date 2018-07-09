39ab792cd397c9e597394025074c5d5527b7ba5b
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-07-09 10:10:19 +0300

    Feature Setting Up GrapghQl

--- server/graphql_schemas/schema.py
+++ server/graphql_schemas/schema.py
@@ -1,21 +1,11 @@
 import graphene
 from graphene_django.debug import DjangoDebug
 
-from .category.schema import Query as CategoryQuery
-from .interest.schema import Query as InterestQuery
-
-
-class Query(
-  CategoryQuery,
-  InterestQuery,
-  graphene.ObjectType,
-):
-  debug = graphene.Field(DjangoDebug, name='__debug')
-  pass
-
+class Query(graphene.ObjectType):
+    debug = graphene.Field(DjangoDebug, name='__debug')
+    pass
 
 class Mutation(graphene.ObjectType):
-  pass
-
+    pass
 
 schema = graphene.Schema(query=Query)
--- server/graphql_schemas/urls.py
+++ server/graphql_schemas/urls.py
@@ -1,12 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.conf.urls import url
 from graphene_django.views import GraphQLView
 
-
-class AuthenticatedGraphView(GraphQLView, LoginRequiredMixin):
-  pass
-
-
 urlpatterns = [
-  url(r'^', AuthenticatedGraphView.as_view(graphiql=True)),
-]
+  url(r'^', GraphQLView.as_view(graphiql=True)),
+ ]
