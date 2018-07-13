fd00ff651a792e795a7135c57202cb78437ff212
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-07-13 17:46:35 +0300

    [Feature #158761270] Adds implementation for updating an event on GraphQL

--- server/graphql_schemas/event/schema.py
+++ server/graphql_schemas/event/schema.py
@@ -3,6 +3,7 @@ import graphene
 from graphene import relay, ObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
+from graphql_relay import from_global_id
 from graphql import GraphQLError
 
 from api.models import Event, Category, AndelaUserProfile
@@ -24,34 +25,72 @@ class CreateEvent(relay.ClientIDMutation):
         date = graphene.String(required=True)
         time = graphene.String(required=False)
         featured_image = graphene.String(required=False)
-        social_event_id = graphene.Int(required=False)
+        social_event_id = graphene.String(required=False)
+
     new_event = graphene.Field(EventNode)
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
         user = info.context.user
         social_event_id = input.get('social_event_id')
-        social_event = Category.objects.get(id=int(social_event_id))
-        andela_user_profile = AndelaUserProfile.objects.get(user_id=user.id)
+        social_event = Category.objects.get(
+            id=from_global_id(social_event_id)[1])
+        user = info.context.user
+        googleUser = GoogleUser.objects.first()
         new_event = Event(
-          title=input.get('title'),
-          description=input.get('description'),
-          venue=input.get('venue'),
-          date=input.get('date'),
-          time=input.get('time'),
-          featured_image=input.get('featured_image'),
-          creator=andela_user_profile,
-          social_event=social_event
+            title=input.get('title'),
+            description=input.get('description'),
+            venue=input.get('venue'),
+            date=input.get('date'),
+            time=input.get('time'),
+            featured_image=input.get('featured_image'),
+            creator=googleUser,
+            social_event=social_event
         )
         new_event.save()
 
         return cls(new_event=new_event)
 
 
+class UpdateEvent(relay.ClientIDMutation):
+
+    class Input:
+        title = graphene.String()
+        description = graphene.String()
+        venue = graphene.String()
+        date = graphene.String()
+        time = graphene.String()
+        featured_image = graphene.String()
+        social_event_id = graphene.Int()
+        event_id = graphene.String(required=True)
+
+    updated_event = graphene.Field(EventNode)
+
+    def update_instance(instance, args, exceptions=['event_id']):
+        if instance:
+            [setattr(instance, key, value)
+             for key, value in args.items() if key not in exceptions]
+        instance.save()
+        return instance
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+
+        try:
+            event_instance = Event.objects.get(
+                id=from_global_id(input.get('event_id'))[1]
+                )
+            if event_instance:
+                updated_event = cls.update_instance(
+                    event_instance, input)
+                return UpdateEvent(updated_event=updated_event)
+        except ValueError as e:
+            # return an error if something wrong happens
+            raise GraphQLError("{}".format(e))
+
+
 class EventQuery(object):
-    event = graphene.Field(EventNode,
-                           id=graphene.Int(),
-                           title=graphene.String())
+    event = relay.Node.Field(EventNode)
     events_list = DjangoFilterConnectionField(EventNode)
 
     def resolve_event(self, info, **kwargs):
