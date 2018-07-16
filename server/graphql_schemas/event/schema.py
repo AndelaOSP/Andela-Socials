96d5d07832b7bbe4ed9eea231c8e8a6b094ea51d
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-07-16 21:55:43 +0300

    [Feature #158761270] 1. Adds Tests for updating an event 2. Adds functionality to catch unauthorized editing of an event.

--- server/graphql_schemas/event/schema.py
+++ server/graphql_schemas/event/schema.py
@@ -1,13 +1,13 @@
 import graphene
 
 from graphene import relay, ObjectType
+from graphql_relay.node.node import from_global_id
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
-from graphql_relay import from_global_id
 from graphql import GraphQLError
 
 from api.models import Event, Category, AndelaUserProfile
-from graphql_schemas.utils.helpers import is_not_admin
+from graphql_schemas.utils.helpers import is_not_admin, update_instance
 
 
 class EventNode(DjangoObjectType):
@@ -31,23 +31,27 @@ class CreateEvent(relay.ClientIDMutation):
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
-        user = info.context.user
         social_event_id = input.get('social_event_id')
-        social_event = Category.objects.get(
-            id=from_global_id(social_event_id)[1])
-        user = info.context.user
-        googleUser = GoogleUser.objects.first()
-        new_event = Event(
-            title=input.get('title'),
-            description=input.get('description'),
-            venue=input.get('venue'),
-            date=input.get('date'),
-            time=input.get('time'),
-            featured_image=input.get('featured_image'),
-            creator=googleUser,
-            social_event=social_event
-        )
-        new_event.save()
+        try:
+            social_event = Category.objects.get(
+                id=from_global_id(social_event_id)[1])
+            user_profile = AndelaUserProfile.objects.get(
+                user=info.context.user
+                )
+            new_event = Event(
+                title=input.get('title'),
+                description=input.get('description'),
+                venue=input.get('venue'),
+                date=input.get('date'),
+                time=input.get('time'),
+                featured_image=input.get('featured_image'),
+                creator=user_profile,
+                social_event=social_event
+            )
+            new_event.save()
+
+        except ValueError as e:
+            raise GraphQLError("An Error occurred. \n{}".format(e))
 
         return cls(new_event=new_event)
 
@@ -61,50 +65,41 @@ class UpdateEvent(relay.ClientIDMutation):
         date = graphene.String()
         time = graphene.String()
         featured_image = graphene.String()
-        social_event_id = graphene.Int()
+        social_event_id = graphene.String()
         event_id = graphene.String(required=True)
 
     updated_event = graphene.Field(EventNode)
-
-    def update_instance(instance, args, exceptions=['event_id']):
-        if instance:
-            [setattr(instance, key, value)
-             for key, value in args.items() if key not in exceptions]
-        instance.save()
-        return instance
+    action_message = graphene.String()
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
 
         try:
+            user = AndelaUserProfile.objects.get(user=info.context.user)
             event_instance = Event.objects.get(
-                id=from_global_id(input.get('event_id'))[1]
-                )
+                pk=from_global_id(input.get('event_id'))[1]
+            )
+            if event_instance.creator != user \
+                    and not info.context.user.is_superuser:
+                raise GraphQLError(
+                    "You are not authorized to edit this event.")
+            if input.get("social_event_id"):
+                input["social_event"] = Category.objects.get(
+                    pk=from_global_id(input.get('social_event_id'))[1]
+                    )
             if event_instance:
-                updated_event = cls.update_instance(
-                    event_instance, input)
-                return UpdateEvent(updated_event=updated_event)
+                updated_event = update_instance(
+                    event_instance,
+                    input,
+                    exceptions=["social_event_id", "event_id"]
+                )
+                return cls(
+                    action_message="Event Update is successful.",
+                    updated_event=updated_event
+                )
         except ValueError as e:
             # return an error if something wrong happens
-            raise GraphQLError("{}".format(e))
-
-
-class EventQuery(object):
-    event = relay.Node.Field(EventNode)
-    events_list = DjangoFilterConnectionField(EventNode)
-
-    def resolve_event(self, info, **kwargs):
-        id = kwargs.get('id')
-
-        if id is not None:
-            event = Event.objects.get(pk=id)
-            if not event.active:
-                return None
-            return event
-        return None
-
-    def resolve_events_list(self, info, **kwargs):
-        return Event.objects.exclude(active=False)
+            raise GraphQLError("An Error occurred. \n{}".format(e))
 
 
 class DeactivateEvent(relay.ClientIDMutation):
@@ -128,6 +123,25 @@ class DeactivateEvent(relay.ClientIDMutation):
         return cls(action_message="Event deactivated")
 
 
+class EventQuery(object):
+    event = relay.Node.Field(EventNode)
+    events_list = DjangoFilterConnectionField(EventNode)
+
+    def resolve_event(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            event = Event.objects.get(pk=id)
+            if not event.active:
+                return None
+            return event
+        return None
+
+    def resolve_events_list(self, info, **kwargs):
+        return Event.objects.exclude(active=False)
+
+
 class EventMutation(ObjectType):
     create_event = CreateEvent.Field()
     deactivate_event = DeactivateEvent.Field()
+    update_event = UpdateEvent.Field()
