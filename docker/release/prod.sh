42863b3e252bb800ddeda82447f6c0ab3e3e109f
natalie-elizabeth <natalie.elizabeth@andela.com>
2018-09-12 10:33:34 +0300

    change

---
+++ docker/release/prod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export SECRET_KEY=whatevetryouwant
+export DATABASE_URL=postgresql://a_social:a_social@database:5432/a_social
+export OAUTH2_CLIENT_ID=1023621061664-1b7grp47bee4qu0k0a5114dvm1icl65k.apps.googleusercontent.com
+export OAUTH2_CLIENT_SECRET=mWqS9LFB2wVqeYz8QTInWwKH
+export NODE_ENV=development
