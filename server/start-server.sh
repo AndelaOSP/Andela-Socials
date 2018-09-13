0ae148024a96d8267bedd8b58d65aad4f6b5eebd
Mayowa Makinde <mankinde53@yahoo.com>
2018-09-13 22:45:23 +0100

    update url to work with docker

--- server/start-server.sh
+++ server/start-server.sh
@@ -4,10 +4,9 @@ set -o pipefail # if any code doesn't return 0, exit the script
 
 
 function setup_server() {
-  source venv/bin/activate
-  python server/manage.py makemigrations
-  python server/manage.py migrate
-  echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('devadmin', 'admin@example.com', 'nimda')" | python server/manage.py shell
+  python manage.py makemigrations
+  python manage.py migrate
+  echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('devadmin', 'admin@example.com', 'nimda')" | python manage.py shell
   if [ $ENVIRONMENT = "production" ]; then
     python manage.py collectstatic --noinput
   fi
@@ -21,7 +20,7 @@ function start_server() {
       --workers 3
   else
     echo Starting Django development server..
-    python server/manage.py runserver 0.0.0.0:8000
+    python manage.py runserver 0.0.0.0:8000
   fi
 }
 
