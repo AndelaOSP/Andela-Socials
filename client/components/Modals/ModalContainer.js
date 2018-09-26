ab8ad2d1b7a25e13c9729a0f218da65d3071b409
Jean Abayo <abagammy@gmail.com>
2018-09-26 10:51:05 +0300

    [Chore #159402806] Fix CC warning

--- client/components/Modals/ModalContainer.js
+++ client/components/Modals/ModalContainer.js
@@ -14,12 +14,8 @@ import EventForm from '../Forms/EventForm';
 const MODAL_COMPONENTS = { CREATE_EVENT: EventForm };
 
 const ModalContent = (props) => {
-  const {
-    modalProps,
-    closeModal,
-    activeModal,
-  } = props;
-  const SpecificModal = MODAL_COMPONENTS[activeModal];
+  const { modalProps, closeModal, activeModal } = props;
+  const SpecificModal = MODAL_COMPONENzTS[activeModal];
   return (
     <div className="modal__content">
       <header className="modal__header">
