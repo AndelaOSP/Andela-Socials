1f3f80e236fc10c36bdec7dd8c3bad1263de1b12
Adegoke Femi Elliot <adegokefemi17@gmail.com>
2019-05-13 09:35:31 +0100

    feat(redesign): redesign event card (#194)
    - make slack icon component reusable
      - add image section
      - add details section
      - add slack share button to card
    [Finishes #165853335]

--- client/assets/icons/SlackIcon.js
+++ client/assets/icons/SlackIcon.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React from 'react';
@@ -7,12 +8,20 @@ const SlackIcon = props => (
   <svg
     width= {props.width}
 =======
+=======
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+>>>>>>> feat(redesign): redesign event card (#194)
 import React from 'react';
 
-const SlackIcon = (
+const SlackIcon = props => (
   <svg
+<<<<<<< HEAD
     width= "7%"
 >>>>>>> ft(share-event-165718129) User should be able to share event to specified Slack channel (#182)
+=======
+    width= {props.width}
+>>>>>>> feat(redesign): redesign event card (#194)
     // height= "13px"
     data-icon="slack"
     role="img"
@@ -21,10 +30,14 @@ const SlackIcon = (
   >
     <path
 <<<<<<< HEAD
+<<<<<<< HEAD
       fill={props.color}
 =======
       fill="currentColor"
 >>>>>>> ft(share-event-165718129) User should be able to share event to specified Slack channel (#182)
+=======
+      fill={props.color}
+>>>>>>> feat(redesign): redesign event card (#194)
       d="M94.12 315.1c0 25.9-21.16 47.06-47.06 47.06S0 341 0 315.1c0-25.9 21.16-47.06 47.06-47.06h47.06v47.06zm23.72 0c0-25.9 21.16-47.06 47.06-47.06s47.06 21.16 47.06 47.06v117.84c0 25.9-21.16 47.06-47.06 47.06s-47.06-21.16-47.06-47.06V315.1zm47.06-188.98c-25.9 0-47.06-21.16-47.06-47.06S139 32 164.9 32s47.06 21.16 47.06 47.06v47.06H164.9zm0 23.72c25.9 0 47.06 21.16 47.06 47.06s-21.16 47.06-47.06 47.06H47.06C21.16 243.96 0 222.8 0 196.9s21.16-47.06 47.06-47.06H164.9zm188.98 47.06c0-25.9 21.16-47.06 47.06-47.06 25.9 0 47.06 21.16 47.06 47.06s-21.16 47.06-47.06 47.06h-47.06V196.9zm-23.72 0c0 25.9-21.16 47.06-47.06 47.06-25.9 0-47.06-21.16-47.06-47.06V79.06c0-25.9 21.16-47.06 47.06-47.06 25.9 0 47.06 21.16 47.06 47.06V196.9zM283.1 385.88c25.9 0 47.06 21.16 47.06 47.06 0 25.9-21.16 47.06-47.06 47.06-25.9 0-47.06-21.16-47.06-47.06v-47.06h47.06zm0-23.72c-25.9 0-47.06-21.16-47.06-47.06 0-25.9 21.16-47.06 47.06-47.06h117.84c25.9 0 47.06 21.16 47.06 47.06 0 25.9-21.16 47.06-47.06 47.06H283.1z"
     />
   </svg>
