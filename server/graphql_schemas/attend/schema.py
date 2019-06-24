78dd420cb571818d9da64ae8e4b5bdea99367843
NtaleGrey <Shadikntale@gmail.com>
2019-06-24 18:49:43 +0300

    feat(events): Filter events user is attending
    - add query to fetch events a user is attending
    - Write tests for the functionality
    
    [Delivers #166284735]

--- server/graphql_schemas/attend/schema.py
+++ server/graphql_schemas/attend/schema.py
@@ -54,6 +54,7 @@ class AttendQuery(object):
     event_attendance = relay.Node.Field(AttendNode)
     attenders_list = DjangoFilterConnectionField(AttendNode)
     subscribed_events = graphene.List(AttendNode)
+    attending_list = DjangoFilterConnectionField(AttendNode)
 
     def resolve_subscribed_events(self, info, **kwargs):
         user = info.context.user
@@ -65,6 +66,10 @@ class AttendQuery(object):
             Q(user__user=info.context.user) |
             Q(event__creator__user=info.context.user)
         )
+
+    def resolve_attending_list(self, info, **kwargs):
+        return Attend.objects.filter(user__user=info.context.user,
+                                     status="attending")
 
 
 class AttendMutation(ObjectType):
